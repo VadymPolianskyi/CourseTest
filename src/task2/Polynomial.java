65f29c5480fa88e243be570d8920bddc76fbff2e
vadym <vadym.polyanski@gmail.com>
2016-12-31 15:10:07 +0200

    Correct polynomial with Pascal's triangle again:)

--- src/task2/Polynomial.java
+++ src/task2/Polynomial.java
@@ -2,63 +2,89 @@ package task2;
 
 import task1.PascalsTriangle;
 
-
 public class Polynomial {
+    String process;
+    int power;
+    long result;
+    PascalsTriangle pascalsTriangle;
+
+    private int a, b, n;
 
     public void searchPolynom(int a, int b, int n) {
-        String process;
-        int power;
+        this.a = a;
+        this.b = b;
+        this.n = n;
+        result = 0;
 
-        PascalsTriangle pascalsTriangle = new PascalsTriangle();
 //        коефіцієнтами біля кожної дії будуть відповідні елементи ряду трикутника Паскаля
+        pascalsTriangle = new PascalsTriangle();
         long[] coefficients = pascalsTriangle.rowN(n);
 
-        long result = 0;
-//        кількість дій буде рівна кількості елементів ряду трикутника Паскаля
-        double actLength = coefficients.length;
+//      кількість дій буде рівна кількості елементів ряду трикутника Паскаля
+        double actCount = coefficients.length;
 //      знаходимо центральну дію, після якої змінюється спадання степеня для "a" і
 //        починається зростання степеня для "b"
-        int median = (int) (actLength-1)/2;
+        int median = (int) (actCount-1)/2;
 
-        process = "a^" + n;
-        result += Math.pow(a,n);
-        power = n-1;
-
-
-        for (int i = 1; i < actLength-1; i ++) {
+        firstAction();
+        for (int i = 1; i < actCount-1; i ++) {
             //так як дія, що знаходиться в центрі завжди буде виглядати "коефіцієнт * a^2*b^2",
             //знаходимо її за медіаною дій і виводимо
             if (i == median) {
 //                якщо кількість дій парна, то "центральна" дія повториться двічі
-                if ((actLength/ 2 - (int) actLength/2) == 0) {
-                    process += "+" + coefficients[i] + "*a^2*b^2";
-                    process += "+" + coefficients[i] + "*a^2*b^2";
-                    result += (coefficients[i]*Math.pow(a,2)*Math.pow(b,2))*2;
+                if ((actCount/ 2 - (int) actCount/2) == 0) {
 
-                    power+=2;
+                    medianAction((int) coefficients[i]);
+                    medianAction((int) coefficients[i]);
                     i++;
                 } else {
 //                    якщо ні - один раз
-                    process += "+" + coefficients[i] + "*a^2*b^2";
-                    result += coefficients[i]*Math.pow(a,2)*Math.pow(b,2);
-                    power++;
+                    medianAction((int) coefficients[i]);
                 }
-
             } else if (i < median) {
-//              після "центральної" дії степінь числа b буде щоразу збільшуватись на 1 до n
-                process += "+" + coefficients[i] + "*a^" + power + "*b";
-                result += coefficients[i] * Math.pow(a,power) * b;
-                power--;
+                beforeMedianAction((int) coefficients[i]);
             } else if (i>median) {
-//              перед "центральною" дією степінь числа а буде щоразу зменшуватись від n на 1
-                process += "+" + coefficients[i] +"*a*b^"+power;
-                result += coefficients[i] * a * Math.pow(b,power);
-                power ++;
+
+                postMedianAction((int) coefficients[i]);
             }
         }
+        lastAction();
+        printResult();
+    }
+
+    public void postMedianAction(int coeficient){
+//     після "центральної" дії степінь числа b буде щоразу збільшуватись на 1 до n
+        process += "+" + coeficient +"*a*b^"+power;
+        result += coeficient * a * Math.pow(b,power);
+        power ++;
+    }
+
+    public void beforeMedianAction(int coeficient) {
+//     перед "центральною" дією степінь числа а буде щоразу зменшуватись від n на 1
+        process += "+" + coeficient + "*a^" + power + "*b";
+        result += coeficient * Math.pow(a,power) * b;
+        power--;
+    }
+
+    public void firstAction() {
+        process = "a^" + n;
+        result += Math.pow(a,n);
+        power = n-1;
+    }
+
+    public void lastAction(){
         process += "+b^" + n;
         result += Math.pow(b,n);
+    }
 
+    public void medianAction(int coefficient) {
+        process += "+" + coefficient + "*a^2*b^2";
+        result += coefficient*Math.pow(a,2)*Math.pow(b,2);
+        power++;
+    }
+
+    public void printResult() {
         System.out.println(process + " = " + result + "\n");
+
     }
 }
