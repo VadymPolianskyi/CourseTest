a7e2aa98d295942def7bb10ebce72fcf7a671a8f
vadym <vadym.polyanski@gmail.com>
2017-01-01 11:32:02 +0200

    task4 with coments

--- src/task4/Rental.java
+++ src/task4/Rental.java
@@ -3,17 +3,23 @@ package task4;
 class Rental {
 
     private Kind kind;
-//
+// I changed kind from int to enum Kind
+// because kind = 1, = 2, = 3 - what does it mean? Is it magic numbers?))
 
     private int days;
 
     private double basePrice;
+
+//  I have added WEEK to good redability of code
+    public final int WEEK = 7;
 
 // Other fields, constructors, get, set, etc. //
 
     public Rental(Kind kind, int days, double basePrice) {
         this.kind=kind;
         this.days=days;
+//        have added base price to constructor
+//        we use it in the method "amountFor" but we dont have any values
         this.basePrice = basePrice;
     }
 
@@ -60,6 +66,10 @@ class Rental {
                 break;
             default:
                 break;
+        }
+
+        if (rental.getDays()>WEEK) {
+            result=result*3;
         }
 // Other calculation.
 
