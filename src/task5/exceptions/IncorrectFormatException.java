9b45731efed74cefc1952c2c6118bb5289731595
vadym <vadym.polyanski@gmail.com>
2017-01-01 13:16:06 +0200

    finished

--- src/task5/exceptions/IncorrectFormatException.java
+++ src/task5/exceptions/IncorrectFormatException.java
@@ -2,9 +2,6 @@ package task5.exceptions;
 
 import java.util.InputMismatchException;
 
-/**
- * Created by vadym on 30.12.16.
- */
 public class IncorrectFormatException extends InputMismatchException {
     public IncorrectFormatException() { super(); }
     public IncorrectFormatException(String message) { super(message); }
